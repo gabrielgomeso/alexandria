41694ed7615e8cd576144b41f8f8dab70e89fe14
Gabriel Gomes <gabrielogomes1@gmail.com>
2024-04-03 20:48:53 -0300

    fix routes and configure logout button

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
+  devise_for :users
+
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
 
   get 'welcome/index'
   get "up" => "rails/health#show", as: :rails_health_check
   resources :books
-  
 
   root 'welcome#index'
 end
