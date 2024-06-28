daeb13f6c4d5e0cbae46a0a665613751fdfac533
Gabriel Gomes <gabrielogomes1@gmail.com>
2024-06-28 17:24:57 -0300

    create book validations and set default cover

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,15 @@
 class Book < ApplicationRecord
     has_one_attached :cover
+    validates :title, presence: true, length: { minimum: 5 }
+    validates :author, presence: true, length: { minimum: 5 }
+    validates :description, presence: true, length: { minimum: 10 }
+
+    before_create :set_default_cover
+
+    private
+  
+    def set_default_cover
+      default_cover_path = Rails.root.join('app', 'assets', 'images', 'default-cover.webp')
+      self.cover.attach(io: File.open(default_cover_path), filename: 'default-cover.webp', content_type: 'image/webp') unless self.cover.attached?
+    end
 end
